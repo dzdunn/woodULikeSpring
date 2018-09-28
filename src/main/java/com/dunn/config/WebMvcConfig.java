b790a938f045b7b10999ad9bae9057b5814a05f2
Danny Dunn <dzdunn@gmail.com>
2018-09-28 23:58:11 +0100

    Enable bootstrap and jquery

--- src/main/java/com/dunn/config/WebMvcConfig.java
+++ src/main/java/com/dunn/config/WebMvcConfig.java
@@ -1,11 +1,15 @@
 package com.dunn.config;
 
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.WebJarsResourceResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
@@ -15,14 +19,17 @@ public class WebMvcConfig implements WebMvcConfigurer{
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/resources/**")
-		.addResourceLocations("/resources/")
-		.setCachePeriod(31556926);
+		registry.addResourceHandler("/static/**")
+		.addResourceLocations("/resources/", "/webjars/")
+		.setCacheControl(
+				CacheControl.maxAge(30L, TimeUnit.DAYS).cachePublic())
+		.resourceChain(true)
+		.addResolver(new WebJarsResourceResolver());
 	}
 
 	@Bean
 	public InternalResourceViewResolver viewResolver() {
-		InternalResourceViewResolver viewResolver= new InternalResourceViewResolver();
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
 		viewResolver.setPrefix("/WEB-INF/jsp/");
 		viewResolver.setSuffix(".jsp");
 		return viewResolver;
--- src/main/webapp/WEB-INF/jsp/home.jsp
+++ src/main/webapp/WEB-INF/jsp/home.jsp
@@ -5,17 +5,36 @@
 <head>
 <meta charset="ISO-8859-1">
 <title>Home Page</title>
+
+<link href="static/bootstrap/css/bootstrap.min.css" type="text/css" rel="stylesheet"/>
+ <script src="static/jquery/dist/jquery.min.js"></script>
+
+
+
+
 </head>
 <body>
 
 	<h1>HOME PAGE</h1>
 	<form action="inputData" method="POST">
 		<label>Type your name: </label> <input type="text" name="name" /> <label>Type
-			your age: </label> <input type="text" name="age" />
-			<input type="submit" value="submit"/>
+			your age: </label> <input type="text" name="age" /> <input type="submit"
+			value="submit" />
 	</form>
+	
+<button type="button" class="btn btn-primary">Primary</button>
 
-	<img alt="graduation" src="resources/static/graduation.jpg"/>
+<img alt="grad" src="static/img/picard.jpg">
+
+<script type="text/javascript">
+//alert("hello");
+
+$(document).ready(function(){
+	
+	alert("works");
+});
+
+</script>
 
 </body>
 </html>
